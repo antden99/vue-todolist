3b6e10a1688caebb5ecebd200ffd48208f9a54fb
antden99 <antoniodenigris@icloud.com>
2024-03-13 15:18:28 +0100

    add listTodo in js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,36 @@
 console.log("tutto ok");
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            listTodo: [
+                {
+                    text: "Melanzana",
+                    done: true,
+                },
+                {
+                    text: "Cipolla",
+                    done: true,
+                },
+                {
+                    text: "Banana",
+                    done: true,
+                },
+                {
+                    text: "Pera",
+                    done: true,
+                },
+                {
+                    text: "Prezzemolo",
+                    done: true,
+                },
+            ]
+        }
+
+    },
+
+
+
+}).mount("#app")
