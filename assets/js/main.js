656daca54cded9d1191a6e1e9eaa869978cd8b83
antden99 <antoniodenigris@icloud.com>
2024-03-13 18:31:03 +0100

    add milestone 3 + bonus 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,19 +26,41 @@ createApp({
                     text: "Prezzemolo",
                     done: true,
                 },
-            ]
+            ],
+            areaInput: "",
+
         }
 
     },
-    methods:{
-        remove(index){
+    methods: {
+        remove(index) {
             //faccio il console log per vedere la stringa rimosso e su quale elemento sto cliccando la X tramite l'indice
-            console.log("rimosso",index);
+            console.log("rimosso", index);
             //richiamo il metodo splice per rimuovere l'elemento dall'array listTodo
-            this.listTodo.splice(index,1)
+            this.listTodo.splice(index, 1)
+        },
+        addTodo() {
+            console.log("aggiungo questa todo", this.areaInput)
+            if (this.areaInput.length > 3) {
+                const newTodo = {
+                    text: this.areaInput,
+                    done: false,
+                };                
+
+                //faccio il console log del nuovo oggetto
+                console.log(newTodo)
+                //assegno il nuovo oggetto all'inizio della lista Todo utilizzando unshift
+                this.listTodo.unshift(newTodo);
+                // svuoto area input
+                this.areaInput=""
+                //controllo la nuova lista
+                console.log(this.listTodo)
+            }
         }
-    }
+    },
+
 
 
 
 }).mount("#app")
+
